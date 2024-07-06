b35f33d7cbf5d420ff804cec0879499939c12693
koyya-suchitra <suchitrakoyya@gmail.com>
2024-07-06 11:27:02 +0530

    a simple calculator using python

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,18 +1,23 @@
 def calculator():
-   a=float(input("enter the vlue of a:"))
-   b=float(input("enter the value ofb:"))
-   operator=input("enter the operation to be performed-(+,-,*,/,%):")
-   if operator == '+':
-     result=a+b
-   elif operator == '-':
-     result=a-b
-   elif operator == '*':
-     result=a*b
-   elif operator == '/':
-     result=a/b
-   elif operator == '%':
-     result=a%b
-   else:
-     result='invalid'
-   print(a,operator,b, "is",result)
+    try:
+        a=float(input("enter the value of a:"))
+        b=float(input("enter the value of b:"))
+        operator=input("enter the operation to be performed,(+,-,*,/,%):")
+        if operator == '+':
+            result=a+b
+        elif operator == '-':
+            result=a-b
+        elif operator == '*':
+            result=a*b
+        elif operator == '/':
+            result=a/b
+        elif operator == '%':
+            result=a%b
+        else:
+            result='invalid'
+        print(a,operator,b, "is",result)
+    except ZeroDivisionError:
+        print("you cannot divide by zero")
+    except ValueError:
+        print("enter only numbers")
 calculator()
---
+++ suchi2/calci.py
@@ -0,0 +1,30 @@
+def calculator():
+        try:
+            a = float(input("Enter the value of a: "))
+            b = float(input("Enter the value of b: "))
+            operator = input("Enter the operation to be performed (+, -, *, /, %): ")
+        
+            if operator == '+':
+                result = a + b
+            elif operator == '-':
+                result = a - b
+            elif operator == '*':
+                result = a * b
+            elif operator == '/':
+                result = a / b
+            elif operator == '%':
+                result = a % b
+            else:
+                result = 'Invalid operation'
+        
+            print(f"{a} {operator} {b} is {result}")
+    
+        except ZeroDivisionError:
+            print("You cannot divide by zero.")
+    
+        except ValueError:
+            print("Please enter valid numbers.")
+
+calculator()
+
+
